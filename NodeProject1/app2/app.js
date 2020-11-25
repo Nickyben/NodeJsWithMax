60a19aa4c4f4999160faf02867c2b46debd9971a
Nicholas Ikechukwu <65307193+Nickyben@users.noreply.github.com>
2020-11-25 14:31:46 +0100

    Learning Node continues 25-11-2020

---
+++ NodeProject1/app2/app.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+
+app.use((req,res,next)=>{
+  res.status(404).send(`
+  <h1>Ouch!! Page Not Found...</h1>
+  `)
+})
+
+app.listen(3000)
