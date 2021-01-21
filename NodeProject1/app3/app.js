6751ab4d77b2ddbe436bf74afd290e3002e9532d
Nicholas Ikechukwu <65307193+Nickyben@users.noreply.github.com>
2021-01-21 23:30:42 +0100

    Jan 20, 2021 lesson
    learning Node.js continues

--- NodeProject1/app3/app.js
+++ NodeProject1/app3/app.js
@@ -5,8 +5,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+
+//const expressHbs = require('express-handlebars'); //import the installed engine FOR HANDLEBARS
+
+//app.engine('handlebars', expressHbs()); //FOR HANDLEBARS
+
+
+
 //SETTING APP GLOBAL VARIABLES WITH EXPRESS for out templating engine and templates folder
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+//app.set('view engine', 'pug');
+
 app.set('views', 'views'); //2nd param is our templates folder
 
 const adminRoutes = require('./routes/admin');
@@ -25,7 +34,8 @@ app.use('/', shopRoutes);
 //IF THE REQUEST IS NOT MET BY ANY OF THE ROUTES ABOVE
 app.use((req, res, next) => {
 
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); //the './' is optional
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    //res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); //the './' is optional
 
     // res.status(404).send(`
     // <h1>Ouch!! Page Not Found...</h1>
--- NodeProject1/app3/routes/admin.js
+++ NodeProject1/app3/routes/admin.js
@@ -4,25 +4,29 @@ const path = require('path');
 const rootDir = require('../util/path');
 const products = [];
 
-
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(
-        path.join(rootDir, 'views', 'add-product.html') //builds the path to work on both windows and linux systems
-        //instead of path.join(__dirname, '../', 'views', 'add-product.html')//builds the path to work on both windows and linux systems
+	res.render('add-product', {
+		pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true, 
+        activeAddProduct: true,
+	});
+	// res.sendFile(
+	//     path.join(rootDir, 'views', 'add-product.html') //builds the path to work on both windows and linux systems
+	//     //instead of path.join(__dirname, '../', 'views', 'add-product.html')//builds the path to work on both windows and linux systems
 
-    )
+	// )
 });
 
 // /admin/add-product =>POST
 router.post('/add-product', (req, res, next) => {
-    products.push({
-        title: req.body.title
-    })
-    res.redirect('/');
+	products.push({
+		title: req.body.title,
+	});
+	res.redirect('/');
 });
-
-
 
 exports.routes = router;
 exports.products = products;
